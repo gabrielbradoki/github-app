ba6a64fcf6b8ec213c5532c97f0267a441ca8c90
Gabriel Araujo <gabrielbradoki@gmail.com>
2020-08-12 23:13:31 -0300

    39. M1#A45 - GitHub app - sobre eventos - desabilitar  habilitar campo de busca

--- src/app.js
+++ src/app.js
@@ -14,12 +14,21 @@ class App extends Component {
     }
   }
 
+  getGithubApiUrl (username, type) {
+    const internalUser = username ? `/${username}` : ''
+    const internalType = type ? `/${type}` : ''
+    return `https://api.github.com/users${internalUser}${internalType}`
+  }
+
   handleSearch (e) {
     const value = e.target.value
     const keyCode = e.which || e.keyCode
     const ENTER = 13
+    const target = e.target
+
     if (keyCode === ENTER) {
-      ajax().get(`https://api.github.com/users/${value}`).then((result) => {
+      target.disabled = true
+      ajax().get(this.getGithubApiUrl(value)).then((result) => {
         this.setState({
           userinfo: {
             username: result.name,
@@ -28,14 +37,39 @@ class App extends Component {
             repos: result.public_repos,
             followers: result.followers,
             following: result.following
-          }
+          },
+          repos: [],
+          starred: []
+        })
+      }).always(() => {
+        target.disabled = false
+      })
+    }
+  }
+
+  getRepos (type) {
+    return (e) => {
+      const username = this.state.userinfo.login
+      ajax().get(this.getGithubApiUrl(username, type)).then((result) => {
+        this.setState({
+          [type]: result.map((repo) => ({
+            name: repo.name,
+            link: repo.html_url
+          }))
         })
       })
     }
   }
 
   render () {
-    return <AppContent userinfo={this.state.userinfo} repos={this.state.repos} starred={this.state.starred} handleSearch={(e) => this.handleSearch(e)} />
+    return <AppContent
+      userinfo={this.state.userinfo}
+      repos={this.state.repos}
+      starred={this.state.starred}
+      handleSearch={(e) => this.handleSearch(e)}
+      getRepos={this.getRepos('repos')}
+      getStarred={this.getRepos('starred')}
+    />
   }
 }
 
